3fc8d044dc5f47c85f71e1cf213a3226a2e2442e
Adrian Iacob <adrian@oc2627300782.ibm.com>
2020-05-25 21:58:41 +0300

    adaug aplicatia

---
+++ app/src/main/java/com/utm/myproject/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.utm.myproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Button addBtn = (Button) findViewById (R.id.addBtn)
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText)
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText)
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView)
+
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString())
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString())
+                int result = num1 + num2
+                resultTextView.setText(result + "")
+
+            }
+        })
+    }
+}
